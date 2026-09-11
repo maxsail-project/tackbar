35d3fe58996e55d766ab14cb8f0655dfe879e563
Maximiliano Mannise <maxsail.project@gmail.com>
2026-09-11 13:17:24 +0200

    style: use Positron basemap in Session Viewer

--- frontend/src/config/map.ts
+++ frontend/src/config/map.ts
@@ -1,4 +1,4 @@
-export const MAP_STYLE_URL = 'https://tiles.openfreemap.org/styles/liberty'
+export const MAP_STYLE_URL = 'https://tiles.openfreemap.org/styles/positron'
 
 export const INITIAL_MAP_VIEW = {
   longitude: -30.75,
